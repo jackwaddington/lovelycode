982326f7083339ef31d4b04c7e72597ab0ea593e
Jack Waddington <jack@waddington.me>
2024-02-05 20:47:13 +0200

    wooeee

--- header.h
+++ header.h
@@ -1,27 +1,17 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   fd.c                                               :+:      :+:    :+:   */
+/*   header.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jwadding <jwadding@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/02/05 14:13:39 by jwadding          #+#    #+#             */
-/*   Updated: 2024/02/05 20:46:09 by jwadding         ###   ########.fr       */
+/*   Created: 2024/02/05 20:26:11 by jwadding          #+#    #+#             */
+/*   Updated: 2024/02/05 20:26:44 by jwadding         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-#include<stdio.h>
-#include<unistd.h>
-#include<fcntl.h>
-int	main()
-{
-	int fd;
-	char text[] ="test";
 
-	printf("The dtable size is %d\nRead more \"man -s2 getddtablesize\"\n\n", getdtablesize());
+#include <stdio.h>
+#include <unistd.h>
 
-	fd = open("file.txt", O_RDWR | O_CREAT);
-	write(fd, &text, sizeof(text));
-	printf("the result of close is %d\n", close(fd));
-	return(0);
-}
+
 
