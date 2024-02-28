2dadef85206b4f4ba14cca698164195ad2fce196
Jack Waddington <jack@waddington.me>
2024-02-28 22:58:03 +0200

    new string try

---
+++ string.c
@@ -0,0 +1,32 @@
+char *create_string(char *s)
+{
+	char *input = "input";
+	char *rtn;
+
+	rtn = (char) malloc (sizeof input * (sizeof char);
+	return (rtn);
+}
+
+void change_p2(char *s)
+{
+	s[1] += 1;
+}
+
+void print_string(char *s)
+{
+	printf("%s\n", s);
+}
+
+int main(void)
+{
+	char * string;
+	string = null;
+
+//	create_string(string);
+//	print_string(string);
+//	change_p2(string);
+//	print_string(string);
+//	change_p4(string);
+//	free(string);
+}
+
